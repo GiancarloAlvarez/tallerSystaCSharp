7af601780b3b129fcfd61c3b0f0cb4d9c336d05f
hugo522 <jeancarlosalvarezjorge12@gmail.com>
2022-04-29 13:33:15 -0400

    UPDATE

---
+++ TallerMecanico/TallerMecanico/View/SYSTA_MENU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallerMecanico.View
+{
+    public partial class SYSTA_MENU : Form
+    {
+        public SYSTA_MENU()
+        {
+            InitializeComponent();
+        }
+    }
+}
