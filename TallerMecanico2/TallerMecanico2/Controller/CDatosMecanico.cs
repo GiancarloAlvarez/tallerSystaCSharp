d59a1645eef735e0c0e88eab89c0feba4d234d31
Giancarlo <jeancarlosalvarezjorge12@gmail.com>
2022-05-04 09:55:24 -0400

    update :D

--- TallerMecanico2/TallerMecanico2/Controller/CDatosMecanico.cs
+++ TallerMecanico2/TallerMecanico2/Controller/CDatosMecanico.cs
@@ -1,12 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TallerMecanico2.Controller
 {
     internal class CDatosMecanico
     {
+        Model.Systa_taller1Entities db;
+
+        public void Insert(Model.SYSTAS_MECANICO Mecanico)
+        {
+            using (db = new Model.Systa_taller1Entities())
+            {
+                try
+                {
+                    db.SYSTAS_MECANICO.Add(Mecanico);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+
+            }
+
+
+        }
+
+        public List<Model.SYSTAS_MECANICO> Read()
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_MECANICO.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+
+
+        }
+
+
+        public void update(Model.SYSTAS_MECANICO Mecanico)
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    db.Entry(Mecanico).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        public void Delete(int sID)
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+                    db.SYSTAS_CLIENTES.Remove(db.SYSTAS_CLIENTES.Single(s => s.ID_Cliente == sID));
+                    db.SaveChanges();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+        public List<Model.SYSTAS_CLIENTES> Search(int sID)
+        {
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_CLIENTES.Where(s => s.ID_Cliente == sID).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+
+        public List<Model.SYSTAS_CLIENTES> SearchName(String sName)
+        {
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_CLIENTES.Where(s => s.Nombre_Cliente.Contains(sName)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+
     }
 }
--- TallerMecanico2/TallerMecanico2/Controller/CDatosServicio.cs
+++ TallerMecanico2/TallerMecanico2/Controller/CDatosServicio.cs
@@ -1,12 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TallerMecanico2.Controller
 {
     internal class CDatosServicio
     {
+
+        Model.Systa_taller1Entities db;
+        public void Insert(Model.SYSTAS_SERVICIO Servicio)
+        {
+            using (db = new Model.Systa_taller1Entities())
+            {
+                try
+                {
+                    db.SYSTAS_SERVICIO.Add(Servicio);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+
+            }
+
+        }
+
+
+        public List<Model.SYSTAS_SERVICIO> Read()
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_SERVICIO.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+
+
+        }
+
+        public void update(Model.SYSTAS_SERVICIO Servicio)
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    db.Entry(Servicio).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+
+        public void Delete(int sID)
+        {
+
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+                    db.SYSTAS_SERVICIO.Remove(db.SYSTAS_SERVICIO.Single(s => s.id_servicio == sID));
+                    db.SaveChanges();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+        public List<Model.SYSTAS_SERVICIO> Search(int sID)
+        {
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_SERVICIO.Where(s => s.id_servicio == sID).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+
+        public List<Model.SYSTAS_SERVICIO> SearchName(String sName)
+        {
+            try
+            {
+                using (db = new Model.Systa_taller1Entities())
+                {
+
+                    return db.SYSTAS_SERVICIO.Where(s => s.nombre_servicio.Contains(sName)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
     }
 }
